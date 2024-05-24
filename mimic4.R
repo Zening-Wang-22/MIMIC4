752f59282e5c0e1aa261a4589607287b2918ac88
Zening Wang <zeningwang@MacBook-Pro-3.local>
2024-05-24 17:25:24 -0400

    add more features

--- mimic4.R
+++ mimic4.R
@@ -19,7 +19,7 @@ library(dplyr)
 projectid = "marine-guard-420203"#replace with your own project id
 bigrquery::bq_auth()#login with google account associated with physionet account
 
-#################### 1
+#################### 1 ICU
 sql1 <- "
 SELECT sepsis.*,icu.hadm_id,icu.intime,icu.outtime,icu.los FROM `physionet-data.mimiciv_derived.sepsis3` sepsis
 LEFT JOIN `physionet-data.mimiciv_icu.icustays` icu
@@ -29,7 +29,7 @@ bq_data1 <- bq_project_query(projectid, query = sql1)
 
 icu_sepsis3 = bq_table_download(bq_data1)
 
-#################### 2
+#################### 2 chemistry
 sql2 <- "
 SELECT * FROM `physionet-data.mimiciv_derived.chemistry`
 WHERE subject_id IN (
@@ -42,7 +42,7 @@ bq_data2 <- bq_project_query(projectid, query = sql2)
 chemistry_sepsis3 = bq_table_download(bq_data2)
 
 
-###################### 3
+###################### 3 CBC
 sql3 <- "
 SELECT * FROM `physionet-data.mimiciv_derived.complete_blood_count`
 WHERE subject_id IN (
@@ -54,14 +54,57 @@ bq_data3 <- bq_project_query(projectid, query = sql3)
 
 cbc_sepsis3 = bq_table_download(bq_data3)
 
+###################### 4 antibiotics
+sql4 <- "
+SELECT * FROM `physionet-data.mimiciv_derived.antibiotic`
+WHERE subject_id IN (
+  SELECT subject_id FROM `physionet-data.mimiciv_derived.sepsis3` 
+)
+AND hadm_id IS NOT NULL"
 
-###################### Join Data Set
-icu_chemistry_cbc_cleaned <- icu_sepsis3 |> 
-  left_join(chemistry_sepsis3, by = "hadm_id") |> 
-  left_join(cbc_sepsis3, by = c("hadm_id", "charttime")) |> 
-  select(-subject_id.x, -subject_id.y, -specimen_id.y) |> 
-  rename(specimen_id = specimen_id.x) |> 
-  select(subject_id, stay_id, hadm_id, specimen_id, everything())
+bq_data4 <- bq_project_query(projectid, query = sql4)
+
+antibiotic_sepsis3 = bq_table_download(bq_data4)
+
+###################### 5 vasopressin
+sql5 <- "
+SELECT * FROM `physionet-data.mimiciv_derived.vasopressin`
+WHERE stay_id IN (
+  SELECT stay_id FROM `physionet-data.mimiciv_derived.sepsis3` 
+)"
+
+bq_data5 <- bq_project_query(projectid, query = sql5)
+vasopressin_sepsis3 = bq_table_download(bq_data5)
+
+
+###################### 6 vitalsign
+sql6 <- "
+SELECT * FROM `physionet-data.mimiciv_derived.vitalsign`
+WHERE subject_id IN (
+  SELECT subject_id FROM `physionet-data.mimiciv_derived.sepsis3` 
+)"
+
+bq_data6 <- bq_project_query(projectid, query = sql6)
+vitalsign_sepsis3 = bq_table_download(bq_data6)
+
+
+###################### 7 demographics
+sql7 <- "
+SELECT * FROM `physionet-data.mimiciii_demo.admissions`
+WHERE subject_id IN (
+  SELECT subject_id FROM `physionet-data.mimiciv_derived.sepsis3` 
+)"
+
+bq_data7 <- bq_project_query(projectid, query = sql7)
+demo_sepsis3 = bq_table_download(bq_data7)
+
+###################### Join Data Set (will revise later)
+# icu_chemistry_cbc_cleaned <- icu_sepsis3 |> 
+#   left_join(chemistry_sepsis3, by = "hadm_id") |> 
+#   left_join(cbc_sepsis3, by = c("hadm_id", "charttime")) |> 
+#   select(-subject_id.x, -subject_id.y, -specimen_id.y) |> 
+#   rename(specimen_id = specimen_id.x) |> 
+#   select(subject_id, stay_id, hadm_id, specimen_id, everything())
 
 ########################### Save 10 lines
 # icu_chemistry_cbc_cleaned_top10 <- head(icu_chemistry_cbc_cleaned, 10)
